7358a2908eba216c599f5db0c53cd2d56fd28dd6
Chase Moskal <chasemoskal@gmail.com>
2020-01-16 15:10:24 -0800

    security: allow all cors requests without origin
     - if a resource is secured via cors whitelist/blacklist,
       then it means browsers can access the resource if they're
       of the correct origin
    
     - if a browser can supply the right origin, a different client
       can spoof it
    
     - thus, any request lacking the 'origin' header may as well be
       accepted
    
     - cors-secured resources are inherently public;
       cors only prevents abusive websites within the browser,
       it means nothing to other clients

--- source/verify-cors.ts
+++ source/verify-cors.ts
@@ -4,7 +4,9 @@ import {CorsPermissions} from "./interfaces.js"
 export const verifyCors = ({origin, cors}: {
 	origin: string
 	cors: CorsPermissions
-}) => cors.allowed.test(origin) && (cors.forbidden
-	? !cors.forbidden.test(origin)
-	: true
+}): boolean => !origin || (
+	cors.allowed.test(origin) && (cors.forbidden
+		? !cors.forbidden.test(origin)
+		: true
+	)
 )
