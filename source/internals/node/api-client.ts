a312ff84342d53f303eb92ae0154e41494bbc7db
Chase Moskal <chasemoskal@gmail.com>
2020-02-15 17:47:40 -0800

    fix: isomorphic-fetch import

--- source/internals/node/api-client.ts
+++ source/internals/node/api-client.ts
@@ -3,7 +3,7 @@
 import mod from "module"
 const require = mod.createRequire(import.meta.url)
 import * as _fetch from "isomorphic-fetch"
-const fetch = require("fetch") as typeof _fetch
+const fetch = require("isomorphic-fetch") as typeof _fetch
 
 import {signatureSign} from "redcrypto/dist/signature-sign.js"
 import {prepareApiClient} from "../isomorphic/prepare-api-client.js"
