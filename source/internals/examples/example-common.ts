e98ec00acbf403763144c4ebe836c7221e6e8fe8
Chase Moskal <chasemoskal@gmail.com>
2020-09-30 21:03:30 -0700

    refine: examples

--- source/internals/examples/example-common.ts
+++ source/internals/examples/example-common.ts
@@ -1,23 +1,36 @@
 
 import {asApi, ApiShape} from "../../interfaces.js"
 
-export type NuclearApi = ReturnType<typeof makeNuclearApi>
+async function verifyTokens(meta: NuclearMeta): Promise<NuclearMetaPayload> {
+	const valid = (!!meta.appToken && !!meta.accessToken)
+	if (!valid) throw new Error("invalid token")
+	return {appId: "a123", userId: "u123"}
+}
+
+export const makeNuclearApi = () => asApi({
+	reactor: {
+		async generatePower(meta: NuclearMeta, a: number, b: number) {
+			const {appId, userId} = await verifyTokens(meta)
+			return a + b
+		},
+		async radioactiveMeltdown(meta: NuclearMeta) {
+			const {appId, userId} = await verifyTokens(meta)
+			throw new Error("meltdown error")
+		},
+	}
+})
 
 export interface NuclearMeta {
 	appToken: string
 	accessToken: string
 }
 
-export const makeNuclearApi = () => asApi({
-	reactor: {
-		async generatePower(meta: NuclearMeta, a: number, b: number) {
-			return a + b
-		},
-		async radioactiveMeltdown(meta: NuclearMeta) {
-			throw new Error("meltdown error")
-		},
-	}
-})
+export interface NuclearMetaPayload {
+	appId: string
+	userId: string
+}
+
+export type NuclearApi = ReturnType<typeof makeNuclearApi>
 
 export const nuclearShape: ApiShape<NuclearApi> = {
 	reactor: {
--- source/internals/examples/example-server.ts
+++ source/internals/examples/example-server.ts
@@ -2,29 +2,6 @@
 import {apiServer} from "../../api-server.js"
 import {makeNuclearApi, NuclearApi} from "./example-common.js"
 
-/*
-
-// hypothetical simplified api
-// security rules apply to whole apis, not individual topics
-// simpler and less maintenance.. less control.. *is this better?*
-
-const server = await renrakuServer({
-	logger: console,
-	api: makeNuclearApi(),
-	origins: {
-		allowed: /^http\:\/\/localhost\:8\d{3}$/i,
-		forbidden: /\:8989$/i,
-	},
-	signers: {
-		"auth-server.public.key": "X8913u12893712...",
-		"admin-server.public.key": "Z9089023812u2...",
-	},
-})
-
-server.start(8001)
-
-*/
-
 export async function exampleServer() {
 	const api = makeNuclearApi()
 	const server = await apiServer<NuclearApi>({
