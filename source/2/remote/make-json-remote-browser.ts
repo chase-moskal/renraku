fcd810199801484adc52c45a84f79b91a8d95543
Chase Moskal <chasemoskal@gmail.com>
2020-12-28 01:46:28 -0800

    tweak: syntax

--- source/2/remote/make-json-remote-browser.ts
+++ source/2/remote/make-json-remote-browser.ts
@@ -3,10 +3,15 @@ import {makeRemote} from "./make-remote.js"
 import {Topic} from "../types/primitives/topic.js"
 import {GetAuth} from "../types/remote/get-auth.js"
 import {ToShape} from "../types/primitives/to-shape.js"
-import {makeJsonRequester} from "./requesters/make-fetch-json-requester.js"
 import {HttpRequestHeaders} from "../types/http/http-request-headers.js"
+import {makeJsonRequester} from "./requesters/make-fetch-json-requester.js"
 
-export function makeJsonRemoteBrowser<xAuth, xTopic extends Topic<any>>({link, shape, headers, getAuth}: {
+export function makeJsonRemoteBrowser<xAuth, xTopic extends Topic<any>>({
+		link,
+		shape,
+		headers,
+		getAuth,
+	}: {
 		link: string
 		shape: ToShape<xTopic>
 		headers: Partial<HttpRequestHeaders>
