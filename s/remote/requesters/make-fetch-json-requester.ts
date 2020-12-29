298bb4601a593fd2d94b27e26db06ad75eea89cc
Chase Moskal <chasemoskal@gmail.com>
2020-12-28 18:31:01 -0800

    rename: source to s, dist to x

--- s/remote/requesters/make-fetch-json-requester.ts
+++ s/remote/requesters/make-fetch-json-requester.ts
@@ -2,8 +2,8 @@
 import {Requester} from "../../types/remote/requester.js"
 import {jsonHttpRequest} from "../../jsonrpc/json-http-request.js"
 import {parseJsonResponse} from "../../jsonrpc/parse-json-response.js"
+import {HttpRequestHeaders} from "../../types/http/http-request-headers.js"
 import {HttpResponseHeaders} from "../../types/http/http-response-headers.js"
-import {HttpRequestHeaders} from "source/types/http/http-request-headers.js"
 
 export function makeJsonRequester<xAuth>({fetch, headers}: {
 		fetch: typeof window.fetch
