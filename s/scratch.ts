43a527398e4a4f5374025f3d7a62df4f6395e437
Chase Moskal <chasemoskal@gmail.com>
2021-01-05 17:48:32 -0800

    sketch: introduce types for shapes and remotes

--- s/scratch.ts
+++ s/scratch.ts
@@ -1,98 +1,25 @@
 
+import {objectMap} from "./tools/object-map.js"
 import {asTopic} from "./identities/as-topic.js"
-import {Responder} from "./types/api/responder.js"
 import {RemoteProcedureCall} from "./api/make-api.js"
 
+import {Await} from "./types/tools/await.js"
 import {Topic} from "./types/primitives/topic.js"
+import {Responder} from "./types/api/responder.js"
+import {DropFirst} from "./types/tools/drop-first.js"
 import {HttpRequest} from "./types/http/http-request.js"
 import {Procedure} from "./types/primitives/procedure.js"
 import {HttpResponse} from "./types/http/http-response.js"
-import { objectMap } from "./tools/object-map.js"
-import { DropFirst } from "./types/tools/drop-first.js"
-import { Await } from "./types/tools/await.js"
-
-export type AlphaAuth = {token: string}
-export type AlphaMeta = {access: boolean}
-
-export type BravoAuth = {abc: string}
-export type BravoMeta = {tables: boolean}
-
-export type Policy<xRequest, xResponse, xAuth, xMeta> = {
-	responder: Responder<xResponse>
-	parseRequest: (request: xRequest) => Promise<RemoteProcedureCall<xAuth>>
-	processAuth: (auth: xAuth) => Promise<xMeta>
-}
-
-// export type ContextStructure = {
-// 	policy: Policy<any, any, any, any>
-// 	procedure: Topic<any>
-// } | {[key: string]: ContextStructure}
-
-export type ToContext<xMeta, xTopic extends Topic<xMeta>, xPolicy extends Policy<any, any, any, any>> = {
-	[P in keyof xTopic]: xTopic[P] extends Procedure<xMeta, any[], any>
-		? {
-			policy: xPolicy
-			procedure: xTopic[P]
-		}
-		: xTopic[P] extends Topic<xMeta>
-			? ToContext<xMeta, xTopic[P], xPolicy>
-			: never
-}
-
-export function makeContext<xAuth, xMeta>() {
-	return function<xRequest, xResponse, xPolicy extends Policy<xRequest, xResponse, xAuth, xMeta>>(policy: xPolicy) {
-		return function<xTopic extends Topic<xMeta>>(expose: xTopic): ToContext<xMeta, xTopic, xPolicy> {
-			throw new Error("TODO implement")
-			return undefined
-		}
-	}
-}
-
-const responder: Responder<HttpResponse> = {
-	resultResponse: (requestId, result) => ({
-		body: "",
-		status: 123,
-		headers: {"Content-Type": "application/json"},
-	}),
-	errorResponse: (requestId, error) => ({
-		body: "",
-		status: 123,
-		headers: {"Content-Type": "application/json"},
-	}),
-}
-
-const alphaPolicy: Policy<HttpRequest, HttpResponse, AlphaAuth, AlphaMeta> = {
-	responder,
-	parseRequest: async request => undefined,
-	processAuth: async auth => (undefined),
-}
-
-const bravoPolicy: Policy<HttpRequest, HttpResponse, BravoAuth, BravoMeta> = {
-	responder,
-	parseRequest: async request => undefined,
-	processAuth: async auth => undefined,
-}
-
-const alpha = asTopic<AlphaMeta>()({
-	async sum(meta, x: number, y: number) {
-		return x + y
-	},
-})
-
-const bravo = asTopic<BravoMeta>()({
-	async divide(meta, x: number, y: number) {
-		return x / y
-	},
-})
-
 
 //
-// TODO continue this new thread
+// API CONTEXT
 //
+
+export type ParseRequest<xRequest, xAuth> = (request: xRequest) => Promise<RemoteProcedureCall<xAuth>>
 
 export type Comms<xRequest, xResponse, xAuth = any> = {
 	responder: Responder<xResponse>
-	parseRequest: (request: xRequest) => Promise<RemoteProcedureCall<xAuth>>
+	parseRequest: ParseRequest<xRequest, xAuth>
 }
 
 export type Policy2<xAuth, xMeta> = {
@@ -118,6 +45,9 @@ function isObject(value: any) {
 }
 
 export const _context = Symbol()
+export type ContextHint<xAuth> = {
+	auth: xAuth
+}
 
 export type ApiContext<xAuth, xMeta, xTopic extends Topic<xMeta>, xPolicy extends Policy2<xAuth, xMeta>> = {
 	[P in keyof xTopic]: xTopic[P] extends Procedure<xMeta, any[], any>
@@ -125,7 +55,7 @@ export type ApiContext<xAuth, xMeta, xTopic extends Topic<xMeta>, xPolicy extend
 		: xTopic[P] extends Topic<xMeta>
 			? ApiContext<xAuth, xMeta, xTopic[P], xPolicy>
 			: never
-} & {[_context]: true}
+} & {[_context]: ContextHint<xAuth>}
 
 export function prepareApi<xRequest, xResponse>(comms: Comms<xRequest, xResponse>) {
 	return function<xAuth, xMeta>() {
@@ -149,10 +79,52 @@ export function prepareApi<xRequest, xResponse>(comms: Comms<xRequest, xResponse
 	}
 }
 
-const prepareJsonApi = prepareApi<HttpRequest, HttpResponse>({
-	responder,
-	parseRequest: async request => undefined,
+export const jsonResponder: Responder<HttpResponse> = {
+	resultResponse: (requestId, result) => ({
+		body: "",
+		status: 123,
+		headers: {"Content-Type": "application/json"},
+	}),
+	errorResponse: (requestId, error) => ({
+		body: "",
+		status: 123,
+		headers: {"Content-Type": "application/json"},
+	}),
+}
+
+// TODO write standard json parser
+export const parseJsonRequest: ParseRequest<HttpRequest, any> = async request => undefined
+
+export const prepareJsonApi = prepareApi<HttpRequest, HttpResponse>({
+	responder: jsonResponder,
+	parseRequest: parseJsonRequest,
 })
+
+type AlphaAuth = {token: string}
+type AlphaMeta = {access: boolean}
+
+type BravoAuth = {abc: string}
+type BravoMeta = {tables: boolean}
+
+const alpha = asTopic<AlphaMeta>()({
+	async sum(meta, x: number, y: number) {
+		return x + y
+	},
+})
+
+const bravo = asTopic<BravoMeta>()({
+	async divide(meta, x: number, y: number) {
+		return x / y
+	},
+})
+
+const alphaPolicy: Policy2<AlphaAuth, AlphaMeta> = {
+	processAuth: async auth => undefined
+}
+
+const bravoPolicy: Policy2<BravoAuth, BravoMeta> = {
+	processAuth: async auth => undefined
+}
 
 const createContext = () => ({
 	alpha: prepareJsonApi<AlphaAuth, AlphaMeta>()(alphaPolicy, alpha),
@@ -162,37 +134,177 @@ const createContext = () => ({
 	},
 })
 
-createContext().group.alpha2[_context]
+type MyContext = ReturnType<typeof createContext>
 
-function isButtered(value: any) {
-	return isObject(value) && !!value[_butter]
+//
+// SHAPES
+//
+
+export type ApiGroupings = ApiContext<any, any, any, any> | {
+	[key: string]: ApiContext<any, any, any, any> | ApiGroupings
 }
 
-/* TODO implement this ganster-shit
+export type Gravy<xAuth> = {
+	getAuth: () => Promise<xAuth>
+}
 
-const alphaAuthPolicy = {getAuth: async() => ({token: "t123"})}
-const bravoAuthPolicy = {getAuth: async() => ({abc: "abc123"})}
+export const _gravy = Symbol()
 
-const remote = generateRemote<ReturnType<typeof createContext>>({
-	shape: {
-		alpha: {
-			[_gravy]: alphaAuthPolicy,
-			sum: true,
-		},
-		bravo: {
-			[_gravy]: bravoAuthPolicy,
-			divide: true,
-		},
-		group: {
-			alpha2: {
-				[_gravy]: alphaAuthPolicy,
-				sum: true,
-			},
-		},
+export type OnlyButteredProcedures<T> = {
+	[P in keyof T as T[P] extends ButteredProcedure<any, any, any[], any, any> ? P : never]: T[P]
+}
+
+export type ToShape<xGroupings extends ApiGroupings> = {
+	[P in keyof xGroupings]: xGroupings[P] extends ButteredProcedure<any, any, any[], any, any>
+		? true
+		: xGroupings[P] extends ApiGroupings
+			? xGroupings[P] extends ApiContext<any, any, any, any>
+				? {
+					[_gravy]: Gravy<xGroupings[P][typeof _context]["auth"]>,
+				} & {
+					[P2 in keyof OnlyButteredProcedures<xGroupings[P]>]: true
+				}
+				: ToShape<xGroupings[P]>
+			: never
+}
+
+const alphaGravy: Gravy<AlphaAuth> = {
+	getAuth: async() => ({token: "t123"})
+}
+
+const bravoGravy: Gravy<BravoAuth> = {
+	getAuth: async() => ({abc: "abc"})
+}
+
+const shape: ToShape<MyContext> = {
+	alpha: {
+		[_gravy]: alphaGravy,
+		sum: true,
 	},
-})
+	bravo: {
+		[_gravy]: bravoGravy,
+		divide: true,
+	},
+	group: {
+		alpha2: {
+			[_gravy]: alphaGravy,
+			sum: true,
+		}
+	},
+}
 
-*/
+//
+// REMOTES
+//
+
+export type ToRemote<xGroupings extends ApiGroupings> = {
+	[P in keyof xGroupings]: xGroupings[P] extends ButteredProcedure<any, any, any[], any, any>
+		? (...args: DropFirst<Parameters<xGroupings[P]["func"]>>) => ReturnType<xGroupings[P]["func"]>
+		: xGroupings[P] extends ApiGroupings
+			? ToRemote<xGroupings[P]>
+			: never
+}
+
+const remote: ToRemote<MyContext> = <any>{}
+remote.alpha.sum
+
+// export type ToRemote<xContext extends ApiShape> = {
+// 	[P in keyof xContext]: xContext[P] extends ApiContext<any, any, any, any>
+// 		? {
+// 			[P2 in keyof xContext[P]]: xContext[P][P2] extends ButteredProcedure<any, any, any[], any, any>
+// 				? xContext[P][P2]
+// 				: never
+// 		}
+// 		: never
+// }
+
+// export function generateRemote<xContext extends ApiShape>({link, shape}: {
+// 		link: string
+// 		shape: xContext
+// 	}): ToRemote<xContext> {
+// 	return objectMap(shape, (value, key) => {
+
+// 	})
+// }
+
+// const alphaAuthPolicy = {getAuth: async() => ({token: "t123"})}
+// const bravoAuthPolicy = {getAuth: async() => ({abc: "abc123"})}
+
+// const remote = generateRemote<MyContext>({
+// 	link,
+// 	shape,
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+// =====================================================================
+// :::::: THE GRAVEYARD ::::::::::::::::::::::::::::::::::::::::::::::::
+// =====================================================================
+//
+
+// export type Policy<xRequest, xResponse, xAuth, xMeta> = {
+// 	responder: Responder<xResponse>
+// 	parseRequest: (request: xRequest) => Promise<RemoteProcedureCall<xAuth>>
+// 	processAuth: (auth: xAuth) => Promise<xMeta>
+// }
+
+// export type ContextStructure = {
+// 	policy: Policy<any, any, any, any>
+// 	procedure: Topic<any>
+// } | {[key: string]: ContextStructure}
+
+// export type ToContext<xMeta, xTopic extends Topic<xMeta>, xPolicy extends Policy<any, any, any, any>> = {
+// 	[P in keyof xTopic]: xTopic[P] extends Procedure<xMeta, any[], any>
+// 		? {
+// 			policy: xPolicy
+// 			procedure: xTopic[P]
+// 		}
+// 		: xTopic[P] extends Topic<xMeta>
+// 			? ToContext<xMeta, xTopic[P], xPolicy>
+// 			: never
+// }
+
+// export function makeContext<xAuth, xMeta>() {
+// 	return function<xRequest, xResponse, xPolicy extends Policy<xRequest, xResponse, xAuth, xMeta>>(policy: xPolicy) {
+// 		return function<xTopic extends Topic<xMeta>>(expose: xTopic): ToContext<xMeta, xTopic, xPolicy> {
+// 			throw new Error("TODO implement")
+// 			return undefined
+// 		}
+// 	}
+// }
+
+
+
+// const remote = generateRemote<ReturnType<typeof createContext>>({
+// 	shape: {
+// 		alpha: {
+// 			[_gravy]: alphaAuthPolicy,
+// 			sum: true,
+// 		},
+// 		bravo: {
+// 			[_gravy]: bravoAuthPolicy,
+// 			divide: true,
+// 		},
+// 		group: {
+// 			alpha2: {
+// 				[_gravy]: alphaAuthPolicy,
+// 				sum: true,
+// 			},
+// 		},
+// 	},
+// })
 
 
 
