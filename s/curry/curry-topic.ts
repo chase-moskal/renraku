907c097cc3c62a05156ebc48d208ac6d5c1b56b5
Chase Moskal <chasemoskal@gmail.com>
2021-01-11 00:28:52 -0800

    rework: strip-auth replaces curry-topic

--- s/curry/curry-topic.ts
+++ s/curry/curry-topic.ts
@@ -4,14 +4,14 @@ import {isObject} from "../identities/is-object.js"
 import {isFunction} from "../identities/is-function.js"
 
 import {Topic} from "../types/primitives/topic.js"
-import {CurryTopic} from "../types/curry/curry-topic.js"
+import {StripAuth} from "../types/curry/strip-auth.js"
 import {_augment} from "../types/symbols/augment-symbol.js"
 
 export function curryTopic<xAuth>() {
 	return function recurse<xTopic extends Topic<xAuth>>({topic, getAuth}: {
 			topic: xTopic
 			getAuth: () => Promise<xAuth>
-		}): CurryTopic<xAuth, xTopic> {
+		}): StripAuth<xTopic> {
 
 		return objectMap(topic, (value, key) => {
 			if (isFunction(value))
--- s/types/curry/strip-auth.ts
+++ s/types/curry/strip-auth.ts
@@ -4,10 +4,10 @@ import {DropFirst} from "../tools/drop-first.js"
 import {Procedure} from "../primitives/procedure.js"
 import {_context} from "../symbols/context-symbol.js"
 
-export type CurryTopic<xAuth, xTopic extends Topic<xAuth>> = {
+export type StripAuth<xTopic extends Topic<any>> = {
 	[P in keyof xTopic]: xTopic[P] extends Procedure<any, any[], any>
 		? (...args: DropFirst<Parameters<xTopic[P]>>) => ReturnType<xTopic[P]>
 		: xTopic[P] extends Topic<any>
-			? CurryTopic<xAuth, xTopic[P]>
+			? StripAuth<xTopic[P]>
 			: never
 }
