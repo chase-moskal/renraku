ef64428422b91d21814f59643a6d6b6d86bb0f4b
Chase Moskal <chasemoskal@gmail.com>
2021-10-05 18:57:01 -0700

    remotes: cleanup mock remote

--- s/remote/mock-remote.ts
+++ s/remote/mock-remote.ts
@@ -6,7 +6,6 @@ import {ApiContext} from "../types/api/api-context.js"
 import {_context} from "../types/symbols/context-symbol.js"
 import {_descriptor} from "../types/symbols/descriptor-symbol.js"
 import {ProcedureDescriptor} from "../types/api/procedure-descriptor.js"
-import {HttpRequest} from "../types/http/http-request.js"
 
 export function mockRemote<
 		xApiContext extends ApiContext<any, any, any, any>
@@ -54,27 +53,3 @@ export function mockRemote<
 		},
 	}
 }
-
-// export function mockRemote<xApiContext extends ApiContext<any, any, any, any>>(apiContext: xApiContext) {
-// 	return function(
-// 			getMockAuth: xApiContext extends ApiContext<any, infer xAuth, any, any>
-// 				? (() => Promise<xAuth>)
-// 				: never
-// 		): Remote<xApiContext> {
-// 		function recurse(context: ApiContext<any, any, any, any>): any {
-// 			return objectMap(context, value => {
-// 				if (value[_descriptor])
-// 					return async(...args: any[]) => {
-// 						const auth = await getMockAuth()
-// 						const procedure: ProcedureDescriptor<any, any, any, any, any> = value
-// 						return procedure.func(auth, ...args)
-// 					}
-// 				else if (value[_context])
-// 					return recurse(value)
-// 				else
-// 					throw new Error("error mocking api context")
-// 			})
-// 		}
-// 		return recurse(apiContext)
-// 	}
-// }
