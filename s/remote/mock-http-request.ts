0a368e69363dcd587aacc98b12fd0ec6ed9b47f6
Chase Moskal <chasemoskal@gmail.com>
2021-10-20 17:27:10 -0700

    remote: tweak mock-http-request type signature

--- s/remote/mock-http-request.ts
+++ s/remote/mock-http-request.ts
@@ -1,5 +1,7 @@
 
-export function mockHttpRequest({origin}: {origin: string}) {
+import {HttpRequest} from "../types/http/http-request.js"
+
+export function mockHttpRequest({origin}: {origin: string}): HttpRequest {
 	return {
 		method: "post",
 		path: "",
