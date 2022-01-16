eb110c90a196291204a19ba9020db9297187594e
Chase Moskal <chasemoskal@gmail.com>
2022-01-16 04:14:08 -0800

    add: authorization to allow-cors
     - close #13

--- s/http/node-utils/allow-cors.ts
+++ s/http/node-utils/allow-cors.ts
@@ -5,7 +5,7 @@ export function allowCors(requestListener: RequestListener): RequestListener {
 	return async(request, response) => {
 		response.setHeader("Access-Control-Allow-Origin", "*")
 		response.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-		response.setHeader("Access-Control-Allow-Headers", "Content-Type")
+		response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		if (request.method.toUpperCase() === "OPTIONS") {
 			response.statusCode = 200
 			response.end()
