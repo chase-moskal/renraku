01542767cadec28353aa2439145c7f74e9d53746
Chase Moskal <chasemoskal@gmail.com>
2022-05-02 06:42:14 -0700

    refactor: extract make-request-listener

--- s/http/node-server.ts
+++ s/http/node-server.ts
@@ -2,14 +2,12 @@
 import {createServer, RequestListener} from "http"
 
 import {servelet} from "../servelet.js"
-import {stopwatch} from "../tools/stopwatch.js"
+import {Api, Logger} from "../types.js"
 import {allowCors} from "./node-utils/allow-cors.js"
-import {readStream} from "./node-utils/read-stream.js"
 import {healthCheck} from "./node-utils/health-check.js"
-import {respondWithError} from "./node-utils/respond-with-error.js"
 import {colorfulLogger} from "../tools/fancy-logging/colorful-logger.js"
 import {timestampedLogger} from "../tools/fancy-logging/timestamped-logger.js"
-import {Api, JsonRpcRequestWithMeta, JsonRpcResponse, Logger} from "../types.js"
+import {makeRequestListener} from "./node-utils/make-request-listener.js"
 
 export function nodeServer({
 		api,
@@ -27,41 +25,12 @@ export function nodeServer({
 
 	const execute = servelet(api)
 
-	let listener: RequestListener = async(req, res) => {
-		let id = -1
-		try {
-			const body = await readStream(req, maxPayloadSize)
-			const request: JsonRpcRequestWithMeta = JSON.parse(body)
-			id = request.id
-			res.setHeader("Content-Type", "application/json; charset=utf-8")
-			const timer = stopwatch()
-			const result = await execute({
-				meta: request.meta,
-				method: request.method,
-				params: request.params,
-				headers: req.headers,
-			})
-			const duration = timer()
-			res.statusCode = 200
-			res.end(
-				JSON.stringify(<JsonRpcResponse>{
-					jsonrpc: "2.0",
-					id,
-					result,
-				})
-			)
-			logger.log(`🔔 ${request.method}() - ${duration.toFixed(0)}ms`)
-		}
-		catch (error) {
-			logger.error(`🚨 ${error.code ?? 500}`, error.stack)
-			return respondWithError({
-				id,
-				error,
-				res,
-				exposeErrors,
-			})
-		}
-	}
+	let listener: RequestListener = makeRequestListener({
+		logger,
+		exposeErrors,
+		maxPayloadSize,
+		execute,
+	})
 
 	listener = healthCheck("/health", logger, listener)
 	listener = allowCors(listener)
