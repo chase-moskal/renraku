908b3b8c4e1e013a786a57506851eba57f1ef775
Chase Moskal <chasemoskal@gmail.com>
2024-08-14 12:56:35 -0700

    improve: auth provider proxy overrides

--- s/neo/auth/provide.ts
+++ s/neo/auth/provide.ts
@@ -3,8 +3,15 @@ import {AuthFns, AuthUnwrap} from "./types.js"
 
 export function provideAuth<A, Fs extends AuthFns<A>>(auth: A, fns: Fs) {
 	return new Proxy({}, {
-		get: (_, key: string) => async(...p: any[]) => await fns[key](auth, ...p),
-		set: () => { throw new Error("readonly") },
+		get: (target: any, key: string) => {
+			return target[key] ?? (
+				async(...p: any[]) => await fns[key](auth, ...p)
+			)
+		},
+		set: (target: any, key: string, value: any) => {
+			target[key] = value
+			return true
+		},
 	}) as AuthUnwrap<Fs>
 }
 
