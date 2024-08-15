78f5c243c6b24de57ac38e70d6cf60a0ea8f0331
Chase Moskal <chasemoskal@gmail.com>
2024-08-15 10:25:53 -0700

    reorg: archiving old stuff

--- s/index.ts
+++ s/index.ts
@@ -1,13 +1,13 @@
 
-export * from "./core/api.js"
-export * from "./core/errors.js"
-export * from "./core/json-rpc.js"
-export * from "./core/remote.js"
-export * from "./core/service.js"
-export * from "./core/types.js"
-
-export * from "./transports/http/client.js"
-export * from "./transports/websocket/client.js"
+// export * from "./core/api.js"
+// export * from "./core/errors.js"
+// export * from "./core/json-rpc.js"
+// export * from "./core/remote.js"
+// export * from "./core/service.js"
+// export * from "./core/types.js"
+//
+// export * from "./transports/http/client.js"
+// export * from "./transports/websocket/client.js"
 
 export * from "./tools/logging/logger.js"
 export * from "./tools/logging/coloring.js"
