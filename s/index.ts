71a1e2351642d0ff2aca04d4e9ba5614c47958c5
Chase Moskal <chasemoskal@gmail.com>
2024-08-15 10:36:48 -0700

    improve: package exports and tsconfig

--- s/index.ts
+++ s/index.ts
@@ -1,17 +1,14 @@
 
-// export * from "./core/api.js"
-// export * from "./core/errors.js"
-// export * from "./core/json-rpc.js"
-// export * from "./core/remote.js"
-// export * from "./core/service.js"
-// export * from "./core/types.js"
-//
-// export * from "./transports/http/client.js"
-// export * from "./transports/websocket/client.js"
+export * from "./core/errors.js"
+export * from "./core/expose.js"
+export * from "./core/remote-proxy.js"
+export * from "./core/remote.js"
+export * from "./core/types.js"
+
+export * from "./transports/http/client.js"
 
 export * from "./tools/logging/logger.js"
 export * from "./tools/logging/coloring.js"
 export * from "./tools/logging/pretty-logger.js"
 export * from "./tools/logging/disabled-logger.js"
-export * from "./tools/death-with-dignity.js"
 
--- s/node.ts
+++ s/node.ts
@@ -1,5 +1,11 @@
 
 export * from "./index.js"
+
+export * from "./transports/http/node-utils/listener-transforms/allow-cors.js"
+export * from "./transports/http/node-utils/listener-transforms/health-check.js"
+export * from "./transports/http/node-utils/endpoint-listener.js"
+export * from "./transports/http/node-utils/read-stream.js"
 export * from "./transports/http/server.js"
-export * from "./transports/websocket/server.js"
+
+export * from "./tools/death-with-dignity.js"
 
