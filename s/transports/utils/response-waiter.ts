820cf92c2a9dbfeeb535f79f489b67894e35075e
Chase Moskal <chasemoskal@gmail.com>
2024-10-29 11:48:46 -0700

    refactor: response-waiter to use deadline tool

--- s/transports/utils/response-waiter.ts
+++ s/transports/utils/response-waiter.ts
@@ -1,12 +1,12 @@
 
 import {JsonRpc} from "../../comms/json-rpc.js"
-import {RemoteError, RemoteTimeoutError} from "../../core/errors.js"
+import {RemoteError} from "../../core/errors.js"
+import {deadline} from "../../tools/deadline.js"
 import {deferPromise, DeferredPromise} from "../../tools/defer-promise.js"
 
 type Pend = {
 	method: string
 	deferred: DeferredPromise<JsonRpc.Response>
-	clear: () => void
 }
 
 export class ResponseWaiter {
@@ -16,20 +16,13 @@ export class ResponseWaiter {
 
 	wait(id: JsonRpc.Id, method: string) {
 		const deferred = deferPromise<JsonRpc.Response>()
-		const timeoutFn = () => {
-			deferred.reject(new RemoteTimeoutError(id, method, `timed out in ${(this.timeout / 1000).toFixed(1)} seconds`))
-			clear()
-		}
-		const timeoutId = setTimeout(timeoutFn, this.timeout)
-		const clear = () => clearTimeout(timeoutId)
-		this.pending.set(id, {method, deferred, clear})
-		return deferred.promise
+		this.pending.set(id, {method, deferred})
+		return deadline(this.timeout, () => deferred.promise)
 	}
 
 	deliverResponse(response: JsonRpc.Response) {
 		const pend = this.pending.get(response.id)
 		if (pend) {
-			pend.clear()
 			if ("error" in response)
 				pend.deferred.reject(new RemoteError(response.id, pend.method, response.error.message))
 			else
