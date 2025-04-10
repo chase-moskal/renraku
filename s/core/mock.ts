0b441f7f4b13c58f91492d81eedc86441e9128ce
Chase Moskal <chasemoskal@gmail.com>
2025-04-10 15:12:46 -0700

    fix: mock types

--- s/core/mock.ts
+++ s/core/mock.ts
@@ -1,9 +1,10 @@
 
 import {Fns} from "./types.js"
 import {remote} from "./remote.js"
+import {Remote} from "./remote-proxy.js"
 import {endpoint, EndpointOptions} from "./endpoint.js"
 
-export function mock<F extends Fns>(fns: Fns, options?: EndpointOptions) {
+export function mock<F extends Fns>(fns: Fns, options?: EndpointOptions): Remote<F> {
 	return remote<F>(endpoint(fns, options))
 }
 
